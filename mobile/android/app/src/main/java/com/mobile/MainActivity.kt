f9573eb0548dcda70096167e321fbf76df43ab0e
yassenn <ennassine@gmail.com>
2026-01-01 23:52:16 +0100

    feat: fixing the gesture handler

--- mobile/android/app/src/main/java/com/mobile/MainActivity.kt
+++ mobile/android/app/src/main/java/com/mobile/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.mobile
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
 
 class MainActivity : ReactActivity() {
 
@@ -18,5 +20,12 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {
+        override fun createRootView() = RNGestureHandlerEnabledRootView(this@MainActivity)
+      }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
+
